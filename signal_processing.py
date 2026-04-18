0a46e0219d8e14f2b236b2d36fddd5a23a18d525
anhbachiem <dhr151103@gmail.com>
2026-04-18 22:40:05 +0700

    Add signal_processing.py with SignalProcessor class for TDoA signal processing algorithm.

---
+++ signal_processing.py
@@ -0,0 +1,23 @@
+class SignalProcessor:
+    def __init__(self, sensor_data):
+        self.sensor_data = sensor_data
+
+    def detect_impact(self):
+        # Implementation for impact detection
+        pass
+
+    def find_peak_times(self):
+        # Implementation for finding peak times of the signals
+        pass
+
+    def calculate_tdoa(self, peak_times):
+        # Implementation for calculating Time Difference of Arrival (TDoA)
+        pass
+
+    def solve_tdoa(self, tdoa):
+        # Implementation for solving the TDoA to get impact coordinates
+        pass
+
+    def process_impact(self):
+        # Implementation for processing the impact to get coordinates
+        pass
